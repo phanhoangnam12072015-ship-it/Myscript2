07a67b87e6a5ce52df7530bd6188ccbcd10149bc
phanhoangnam12072015-ship-it <phanhoangnam12072015@gmail.com>
2025-12-31 12:47:25 +0700

    Update Scriptdemo.lua

--- Scriptdemo.lua
+++ Scriptdemo.lua
@@ -1,6 +1,9 @@
 -- Load Ui REDz V2
 loadstring(game:HttpGet("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua"))()
 
+-------------------------------------------------
+-- WINDOW + KEY SYSTEM
+-------------------------------------------------
 local Window = MakeWindow({
     Hub = {
         Title = "Demo Hub",
@@ -10,7 +13,7 @@ local Window = MakeWindow({
     Key = {
         KeySystem = true,
         Title = "Key System",
-        Description = "Enter key to continue",
+        Description = "Enter your key to continue",
         Keys = {
             "muhehehe",
             "key123",
@@ -19,81 +22,114 @@ local Window = MakeWindow({
         },
         Notifi = {
             Notifications = true,
-            CorrectKey = "Correct key!",
+            CorrectKey = "Correct key! Saved ✔",
             Incorrectkey = "Wrong key!",
             CopyKeyLink = ""
-        }
+        },
+        SaveKey = true
+    },
+
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "SubinHub",
+        FileName = "KeyConfig"
     }
 })
 
 -------------------------------------------------
 -- TABS
 -------------------------------------------------
-local Tab1o = MakeTab({ Name = "Script Forsaken" })
-local Tab2o = MakeTab({ Name = "Script War Tycoon" })
-local Tab3o = MakeTab({ Name = "Script TSB" })
-local Tab4o = MakeTab({ Name = "Script Blue Block" })
-local Tab5o = MakeTab({ Name = "Script Dead Rail" })
-local Tab6o = MakeTab({ Name = "Script Server VIP" })
-local Tab7o = MakeTab({ Name = "Script Blox Fruit" })
-local Tab8o = MakeTab({ Name = "Script Steal a Brainrot" })
-local Tab9o = MakeTab({ Name = "Script tổng hợp khác" })
+local Tab1 = MakeTab({ Name = "Script Forsaken" })
+local Tab2 = MakeTab({ Name = "Script War Tycoon" })
+local Tab3 = MakeTab({ Name = "Script TSB" })
+local Tab4 = MakeTab({ Name = "Script Blue Block" })
+local Tab5 = MakeTab({ Name = "Script Dead Rail" })
+local Tab6 = MakeTab({ Name = "Script Server VIP" })
+local Tab7 = MakeTab({ Name = "Script Blox Fruit" })
+local Tab8 = MakeTab({ Name = "Script Steal a Brainrot" })
+local Tab9 = MakeTab({ Name = "Script tổng hợp khác" })
 
 -------------------------------------------------
--- BUTTONS
+-- TAB 1
 -------------------------------------------------
-AddButton(Tab1o,{Name="Sigmaken",Callback=function()
+AddButton(Tab1,{Name="Sigmaken",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sigmaboy-sigma-boy/Sigmasaken/refs/heads/main/sigmasakenmain"))()
 end})
 
-AddButton(Tab1o,{Name="Nullsaken",Callback=function()
+AddButton(Tab1,{Name="Nullsaken",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/34f3f/forsaken.github.io/refs/heads/main/ringtabublik.lua"))()
 end})
 
-AddButton(Tab2o,{Name="AWT (with key)",Callback=function()
+-------------------------------------------------
+-- TAB 2
+-------------------------------------------------
+AddButton(Tab2,{Name="AWT (with key)",Callback=function()
 loadstring(game:HttpGet("https://pastebin.com/raw/gf22x6st"))()
 end})
 
-AddButton(Tab2o,{Name="FacilityHub",Callback=function()
+AddButton(Tab2,{Name="FacilityHub",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FacilityHUB/FacilityHub/refs/heads/main/WarTycoons"))()
 end})
 
-AddButton(Tab3o,{Name="VexonHub",Callback=function()
+-------------------------------------------------
+-- TAB 3
+-------------------------------------------------
+AddButton(Tab3,{Name="VexonHub",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/refs/heads/main/VexonHub"))()
 end})
 
-AddButton(Tab4o,{
+-------------------------------------------------
+-- TAB 4
+-------------------------------------------------
+AddButton(Tab4,{
 Name="Exuso Hub",
 Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BexruzScripts/Blue-lock-no-cd-reo-get-any-style-script.-WORKS-WITH-WORLD-CLASSES-/refs/heads/main/script.lua"))()
 end
 })
 
-AddButton(Tab5o,{Name="Skull Hub",Callback=function()
+-------------------------------------------------
+-- TAB 5
+-------------------------------------------------
+AddButton(Tab5,{Name="Skull Hub",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/hungquan99/SkullHub/main/loader.lua"))()
 end})
 
-AddButton(Tab6o,{Name="Server VIP Free",Callback=function()
+-------------------------------------------------
+-- TAB 6
+-------------------------------------------------
+AddButton(Tab6,{Name="Server VIP Free",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/anuragaming1/Meow_gaming/refs/heads/main/Servervip.lua"))()
 end})
 
-AddButton(Tab7o,{Name="QuantumOnyx",Callback=function()
+-------------------------------------------------
+-- TAB 7 (BLOX FRUIT)
+-------------------------------------------------
+AddButton(Tab7,{Name="QuantumOnyx",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/flazhy/QuantumOnyx/refs/heads/main/QuantumOnyx.lua"))()
 end})
 
-AddButton(Tab8o,{Name="Spawn Brainrot",Callback=function()
+-- ✅ BLUE X RED (ĐÃ GẮN ĐÚNG)
+AddButton(Tab7,{Name="Blue X Red",Callback=function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))()
+end})
+
+-------------------------------------------------
+-- TAB 8
+-------------------------------------------------
+AddButton(Tab8,{Name="Spawn Brainrot",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/khelzzz/x/refs/heads/main/Sab-Spawner"))()
 end})
 
-AddButton(Tab9o,{Name="Huysad Hub",Callback=function()
+-------------------------------------------------
+-- TAB 9
+-------------------------------------------------
+AddButton(Tab9,{Name="Huysad Hub",Callback=function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HuySadd/Huy-Sad-Ch-nh-Th-c/refs/heads/main/HuysadChinhThuc.lua"))()
 end})
 
-AddButton(Tab7o,{Name="blue x red",Callback=function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-BlueX/BlueX-Hub/refs/heads/main/Main.lua"))()
-end})
 -------------------------------------------------
--- MINIMIZE
+-- MINIMIZE BUTTON
 -------------------------------------------------
 MinimizeButton({
     Image = "https://cdn.discordapp.com/attachments/1418169288434651250/1455439803469402281/image0.jpg",
